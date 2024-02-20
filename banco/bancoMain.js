9f5e6f627eade40b1b2f5daf6c4a2e8d91e41fc9
Gabriel Maiolli <bielmaiolli@gmail.com>
2024-02-20 01:01:10 -0300

    Update bancoMain.js

--- banco/bancoMain.js
+++ banco/bancoMain.js
@@ -1,18 +1,15 @@
-//(verificar saldo, depositar, sacar, proibir compra caso saldo for negativo, etc...)
+//(verificar saldo, depositar, sacar, proibir saque caso saldo for ficar negativo, etc...)
 
-var saldo = 30
+let saldo = 30
+let valor = 0
 
 function verificarSaldo(){
     return `O seu saldo é de ${saldo.toLocaleString('pt-BR',{style:'currency', currency:'BRL'})}`;
 }
 
-//console.log(verificarSaldo())
+//console.log(verificarSaldo()) teste de verificação do saldo
 
 
-
-
-
-var valor = 0
 
 function depositar(valor){
    
@@ -23,8 +20,7 @@ function depositar(valor){
     return `É impossivel depositar um valor negativo ou igual a zero, a tentativa  de deposito foi com o valor ${valor}`
 }
 }
-//console.log(depositar(30))
-
+//console.log(depositar(30)) teste de deposito
 
 
 
@@ -38,26 +34,11 @@ function sacar(valor){
 }
 }
 
-//console.log(sacar(40))
-
-/*
-do{
-       
-   var continuar = prompt("Deseja continuar? (digite apenas Y ou N)")
-    
-   if(continuar==Y){
-    continuar==true
-   } if(continuar==N) {
-    continuar==false
-   } else {
-    return "Erro";
-   }
-
-} while(continuar=false)
-*/
+//console.log(sacar(40)) teste de saque
 
 
- var escolha = 2
+
+ var escolha = 3         // escolher qual operação deseja utilizar
 
  switch (escolha){
 
@@ -72,7 +53,7 @@ do{
     
     case 2:
         //depositar
-        console.log(depositar(-30))
+        console.log(depositar(50))
         break;
 
     case 3: 
@@ -80,6 +61,3 @@ do{
         console.log(sacar(40))
         break;          
  }
-
-
-
